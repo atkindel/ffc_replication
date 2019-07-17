13106648c65f4075381515111f080b2d8d9d91a6
Alex Kindel <alextkindel@gmail.com>
2019-07-17 14:54:59 -0700

    Update master script

--- code/run.sh
+++ code/run.sh
@@ -1,9 +1,29 @@
 #!/bin/sh
 # Measuring the Predictability of Life Outcomes with a Scientific Mass Collaboration   
-# Script to build results (figures, tables)
+# Script to build results (figures, tables) from scratch
+
+# Write log 
+rm -f log.txt
+touch log.txt
+echo "Log from run: $(date)" >> log.txt
 
 # Ian: Transforms all submissions into a single data file
-Rscript ./subset_submissions.R
+Rscript ./subset_submissions.R >> log.txt
 
-# Ian: Generates tables/figures from main paper and supplemental materials
-Rscript ./science_figures.R
+# Ian: Generates several sets of figures:
+#  - Main paper figure 2 (main challenge results)
+#  - SM figure S3 (training data distributions)
+#  - SM figures S7a-d; S8-S11 (performance benchmarks and uncertainty)
+#  - SM figures S12a-f (team scores by outcome)
+#  - SM figure S13 (out-of-sample error estimates)
+#  - SM figure S14 (alternative confidence intervals)
+Rscript ./science_figures.R >> log.txt
+
+# Matt: Generates SM figures S2A, S2B (missing data)
+Rscript ./make_missing_data_graphs.R >> log.txt
+
+# Alex: Generates SM figures S4, S5 (participant demographics)
+Rscript ./participant_analysis.R >> log.txt
+
+# Alex: Generates SM figures S17, S18, 19 (code submission analysis)
+Rscript ./code_analysis.R >> log.txt
