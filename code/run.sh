ac6239b7d913702475ea0774dde1a0cc4add249a
Alex Kindel <alextkindel@gmail.com>
2019-12-19 15:21:52 -0800

    Tee logs to stdout and logfile

--- code/run.sh
+++ code/run.sh
@@ -5,34 +5,34 @@
 # Write a new log 
 rm -f log.txt
 touch log.txt
-echo "Log from run: $(date)" >> log.txt
+echo "Log from run: $(date)" | tee log.txt
 
 ## Package installation ##
 # These are pre-installed on the Docker image; uncomment if running on own machine
-# echo "BEGIN install_requirements.R" >> log.txt
-# Rscript ./install_requirements.R >> log.txt
-# echo "END install_requirements.R" >> log.txt
+# echo "BEGIN install_requirements.R" | tee log.txt
+# Rscript ./install_requirements.R | tee log.txt
+# echo "END install_requirements.R" | tee log.txt
 
 
 ## Data preparation ##
 
 # Ian: Transforms all submissions into a single data file
-echo "BEGIN subset_submissions.R" >> log.txt
-Rscript ./subset_submissions.R >> log.txt
-echo "END subset_submissions.R" >> log.txt
+echo "BEGIN subset_submissions.R" | tee log.txt
+Rscript ./subset_submissions.R | tee log.txt
+echo "END subset_submissions.R" | tee log.txt
 
 # Matt: Transforms Ian submission file into working data frames
-echo "BEGIN prep_predictions.R" >> log.txt
-Rscript ./prep_predictions.R >> log.txt
-echo "END prep_predictions.R" >> log.txt
+echo "BEGIN prep_predictions.R" | tee log.txt
+Rscript ./prep_predictions.R | tee log.txt
+echo "END prep_predictions.R" | tee log.txt
 
 # Alex: Performs data preparation for code analysis (experimental)
 # NOTE: Requires python3 and dependencies specified in requirements.txt
 # This generates ffc_code.csv, but may be buggy depending on your setup, so we don't run it by default
 # Just use the included ffc_code.csv if you don't want to try running this
-# bash ./prep_data.sh >> log.txt
+# bash ./prep_data.sh | tee log.txt
 # pip3 install -r requirements.txt
-# yes | python3 ./code_analysis.py >> log.txt
+# yes | python3 ./code_analysis.py | tee log.txt
 
 ## Reproduce main paper and supplementary material (SM) figures ##
 
@@ -42,65 +42,65 @@ echo "END prep_predictions.R" >> log.txt
 #  - SM figures S6a-d; S7-S10 (performance benchmarks and uncertainty)
 #  - SM figure S11 (out-of-sample error estimates)
 #  - SM figures S15a-f (team scores by outcome)
-echo "BEGIN science_figures.R" >> log.txt
-Rscript ./science_figures.R >> log.txt
-echo "END science_figures.R" >> log.txt
+echo "BEGIN science_figures.R" | tee log.txt
+Rscript ./science_figures.R | tee log.txt
+echo "END science_figures.R" | tee log.txt
 
 # Matt: Generates main paper figure 4 (prediction heatmaps)
 #  Also outputs SM table S7 (model fit for models of prediction error by account, observation)
 #  Generating all of the heatmaps at once requires ~6gb memory
-echo "BEGIN heatmaps.R" >> log.txt
-Rscript ./heatmaps.R >> log.txt
-echo "END heatmaps.R" >> log.txt
+echo "BEGIN heatmaps.R" | tee log.txt
+Rscript ./heatmaps.R | tee log.txt
+echo "END heatmaps.R" | tee log.txt
 
 # Matt: Generates SM figures S1a, S1b (missing data)
 #  Generating the heatmap in S1a requires ~14gb memory
-echo "BEGIN make_missing_data_graphs.R" >> log.txt
-Rscript ./make_missing_data_graphs.R >> log.txt
-echo "END make_missing_data_graphs.R" >> log.txt
+echo "BEGIN make_missing_data_graphs.R" | tee log.txt
+Rscript ./make_missing_data_graphs.R | tee log.txt
+echo "END make_missing_data_graphs.R" | tee log.txt
 
 # Alex: Generates SM figures S3a, S3b, S4 (participant demographics)
-echo "BEGIN participant_analysis.R" >> log.txt
-Rscript ./participant_analysis.R >> log.txt
-echo "END participant_analysis.R" >> log.txt
+echo "BEGIN participant_analysis.R" | tee log.txt
+Rscript ./participant_analysis.R | tee log.txt
+echo "END participant_analysis.R" | tee log.txt
 
 # Matt: Generates SM figure S13 (prediction difficulty in extreme cases)
-echo "BEGIN hard_to_predict_cases.R" >> log.txt
-Rscript ./hard_to_predict_cases.R >> log.txt
-echo "END hard_to_predict_cases.R" >> log.txt
+echo "BEGIN hard_to_predict_cases.R" | tee log.txt
+Rscript ./hard_to_predict_cases.R | tee log.txt
+echo "END hard_to_predict_cases.R" | tee log.txt
 
 # Matt: Generates SM figure S14 (prediction difficulty across outcomes within families)
-echo "BEGIN difficulty_across_outcomes.R" >> log.txt
-Rscript ./difficulty_across_outcomes.R >> log.txt
-echo "END difficulty_across_outcomes.R" >> log.txt
+echo "BEGIN difficulty_across_outcomes.R" | tee log.txt
+Rscript ./difficulty_across_outcomes.R | tee log.txt
+echo "END difficulty_across_outcomes.R" | tee log.txt
 
 # Alex: Generates SM figures S16, S17, S18 (code submission analysis)
-echo "BEGIN code_analysis.R" >> log.txt
-Rscript ./code_analysis.R >> log.txt
-echo "END code_analysis.R" >> log.txt
+echo "BEGIN code_analysis.R" | tee log.txt
+Rscript ./code_analysis.R | tee log.txt
+echo "END code_analysis.R" | tee log.txt
 
 # Matt: Generates SM figure S12 (prediction distances compared to truth)
-echo "BEGIN compare_predictions_truth.R" >> log.txt
-Rscript ./compare_predictions_truth.R >> log.txt
-echo "END compare_predictions_truth.R" >> log.txt
+echo "BEGIN compare_predictions_truth.R" | tee log.txt
+Rscript ./compare_predictions_truth.R | tee log.txt
+echo "END compare_predictions_truth.R" | tee log.txt
 
 # Alex: Generates SM figures S19-22 (methods used in the Challenge)
-echo "BEGIN methods_used.R" >> log.txt
-Rscript ./methods_used.R >> log.txt
-echo "END methods_used.R" >> log.txt
+echo "BEGIN methods_used.R" | tee log.txt
+Rscript ./methods_used.R | tee log.txt
+echo "END methods_used.R" | tee log.txt
 
 # Ian: Generates privacy redaction counts
-echo "BEGIN privacy_redaction_count.R" >> log.txt
-Rscript ./privacy_redaction_count.R >> log.txt
-echo "END privacy_redaction_count.R" >> log.txt
+echo "BEGIN privacy_redaction_count.R" | tee log.txt
+Rscript ./privacy_redaction_count.R | tee log.txt
+echo "END privacy_redaction_count.R" | tee log.txt
 
 # Produces table S4 showing the number of non-missing cases for each outcome in the training, leaderboard, and holdout sets.
-echo "BEGIN num_nonmissing_cases.R" >> log.txt
-Rscript ./num_nonmissing_cases.R >> log.txt
-echo "END num_nonmissing_cases.R" >> log.txt
+echo "BEGIN num_nonmissing_cases.R" | tee log.txt
+Rscript ./num_nonmissing_cases.R | tee log.txt
+echo "END num_nonmissing_cases.R" | tee log.txt
 
 # Remove Rplots.pdf
 rm -f ./Rplots.pdf
 
-echo "Replication code run complete." >> log.txt
-echo "" >> log.txt
+echo "Replication code run complete." | tee log.txt
+echo "" | tee log.txt
--- code/socius/run.sh
+++ code/socius/run.sh
@@ -4,21 +4,31 @@
 # Write a new log 
 rm -f log.txt
 touch log.txt
-echo "Log from run: $(date)" >> log.txt
+echo "Log from run: $(date)" | tee log.txt
 
 # Produces missing data summary plots in Figure 3
-Rscript ./make_missing_data_graphs.R >> log.txt
+echo "BEGIN make_missing_data_graphs.R" | tee log.txt
+Rscript ./make_missing_data_graphs.R | tee log.txt
+echo "END make_missing_data_graphs.R" | tee log.txt
 
 # Produces the table showing the number of non-missing cases for each outcome in the training, leaderboard, and holdout sets.
-Rscript ./num_nonmissing_cases.R >> log.txt
+echo "BEGIN num_nonmissing_cases.R" | tee log.txt
+Rscript ./num_nonmissing_cases.R | tee log.txt
+echo "END make_missing_data_graphs.R" | tee log.txt
 
 # Reports the percent of entries made missing in our privacy and ethics audit.
 # We also cite this number in the community paper.
-Rscript ./privacy_redaction_count.R >> log.txt
+echo "BEGIN privacy_redaction_count.R" | tee log.txt
+Rscript ./privacy_redaction_count.R | tee log.txt
+echo "END make_missing_data_graphs.R" | tee log.txt
 
 # Produces the 3D tree visualization (Figure 5).
-Rscript ./tree_plot.R >> log.txt
+echo "BEGIN tree_plot.R" | tee log.txt
+Rscript ./tree_plot.R | tee log.txt
+echo "END make_missing_data_graphs.R" | tee log.txt
 
 # Produces the empirical CDF of R^2 for each outcome and data set (Figure 6).
 # Also produces the scatterplot of R^2_Training and R^2_Holdout (Figure 7).
-Rscript ./r2_allSubmission_plots.R >> log.txt
+echo "BEGIN r2_allSubmission_plots.R" | tee log.txt
+Rscript ./r2_allSubmission_plots.R | tee log.txt
+echo "END make_missing_data_graphs.R" | tee log.txt
