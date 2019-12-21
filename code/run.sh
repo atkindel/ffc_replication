a96b92ba2eff04bff7d12cd2949ae2452d5650fd
Alex Kindel <alextkindel@gmail.com>
2019-12-20 18:10:18 -0800

    Ensure log file not overwritten

--- code/run.sh
+++ code/run.sh
@@ -5,7 +5,7 @@
 # Write a new log 
 rm -f log.txt
 touch log.txt
-echo "Log from run: $(date)" | tee log.txt
+echo "Log from run: $(date)" | tee -a log.txt
 
 ## Package installation ##
 # These are pre-installed on the Docker image; uncomment if running on own machine
@@ -17,22 +17,22 @@ echo "Log from run: $(date)" | tee log.txt
 ## Data preparation ##
 
 # Ian: Transforms all submissions into a single data file
-echo "BEGIN subset_submissions.R" | tee log.txt
-Rscript ./subset_submissions.R | tee log.txt
-echo "END subset_submissions.R" | tee log.txt
+echo "BEGIN subset_submissions.R" | tee -a log.txt
+Rscript ./subset_submissions.R | tee -a log.txt
+echo "END subset_submissions.R" | tee -a log.txt
 
 # Matt: Transforms Ian submission file into working data frames
-echo "BEGIN prep_predictions.R" | tee log.txt
-Rscript ./prep_predictions.R | tee log.txt
-echo "END prep_predictions.R" | tee log.txt
+echo "BEGIN prep_predictions.R" | tee -a log.txt
+Rscript ./prep_predictions.R | tee -a log.txt
+echo "END prep_predictions.R" | tee -a log.txt
 
 # Alex: Performs data preparation for code analysis (experimental)
 # NOTE: Requires python3 and dependencies specified in requirements.txt
 # This generates ffc_code.csv, but may be buggy depending on your setup, so we don't run it by default
 # Just use the included ffc_code.csv if you don't want to try running this
-# bash ./prep_data.sh | tee log.txt
+# bash ./prep_data.sh | tee -a log.txt
 # pip3 install -r requirements.txt
-# yes | python3 ./code_analysis.py | tee log.txt
+# yes | python3 ./code_analysis.py | tee -a log.txt
 
 ## Reproduce main paper and supplementary material (SM) figures ##
 
@@ -42,65 +42,65 @@ echo "END prep_predictions.R" | tee log.txt
 #  - SM figures S6a-d; S7-S10 (performance benchmarks and uncertainty)
 #  - SM figure S11 (out-of-sample error estimates)
 #  - SM figures S15a-f (team scores by outcome)
-echo "BEGIN science_figures.R" | tee log.txt
-Rscript ./science_figures.R | tee log.txt
-echo "END science_figures.R" | tee log.txt
+echo "BEGIN aggregate_score_analyses.R" | tee -a log.txt
+Rscript ./aggregate_score_analyses.R | tee -a log.txt
+echo "END aggregate_score_analyses.R" | tee -a log.txt
 
 # Matt: Generates main paper figure 4 (prediction heatmaps)
 #  Also outputs SM table S7 (model fit for models of prediction error by account, observation)
 #  Generating all of the heatmaps at once requires ~6gb memory
-echo "BEGIN heatmaps.R" | tee log.txt
-Rscript ./heatmaps.R | tee log.txt
-echo "END heatmaps.R" | tee log.txt
+echo "BEGIN heatmaps.R" | tee -a log.txt
+Rscript ./heatmaps.R | tee -a log.txt
+echo "END heatmaps.R" | tee -a log.txt
 
 # Matt: Generates SM figures S1a, S1b (missing data)
 #  Generating the heatmap in S1a requires ~14gb memory
-echo "BEGIN make_missing_data_graphs.R" | tee log.txt
-Rscript ./make_missing_data_graphs.R | tee log.txt
-echo "END make_missing_data_graphs.R" | tee log.txt
+echo "BEGIN make_missing_data_graphs.R" | tee -a log.txt
+Rscript ./make_missing_data_graphs.R | tee -a log.txt
+echo "END make_missing_data_graphs.R" | tee -a log.txt
 
 # Alex: Generates SM figures S3a, S3b, S4 (participant demographics)
-echo "BEGIN participant_analysis.R" | tee log.txt
-Rscript ./participant_analysis.R | tee log.txt
-echo "END participant_analysis.R" | tee log.txt
+echo "BEGIN participant_analysis.R" | tee -a log.txt
+Rscript ./participant_analysis.R | tee -a log.txt
+echo "END participant_analysis.R" | tee -a log.txt
 
 # Matt: Generates SM figure S13 (prediction difficulty in extreme cases)
-echo "BEGIN hard_to_predict_cases.R" | tee log.txt
-Rscript ./hard_to_predict_cases.R | tee log.txt
-echo "END hard_to_predict_cases.R" | tee log.txt
+echo "BEGIN hard_to_predict_cases.R" | tee -a log.txt
+Rscript ./hard_to_predict_cases.R | tee -a log.txt
+echo "END hard_to_predict_cases.R" | tee -a log.txt
 
 # Matt: Generates SM figure S14 (prediction difficulty across outcomes within families)
-echo "BEGIN difficulty_across_outcomes.R" | tee log.txt
-Rscript ./difficulty_across_outcomes.R | tee log.txt
-echo "END difficulty_across_outcomes.R" | tee log.txt
+echo "BEGIN difficulty_across_outcomes.R" | tee -a log.txt
+Rscript ./difficulty_across_outcomes.R | tee -a log.txt
+echo "END difficulty_across_outcomes.R" | tee -a log.txt
 
 # Alex: Generates SM figures S16, S17, S18 (code submission analysis)
-echo "BEGIN code_analysis.R" | tee log.txt
-Rscript ./code_analysis.R | tee log.txt
-echo "END code_analysis.R" | tee log.txt
+echo "BEGIN code_analysis.R" | tee -a log.txt
+Rscript ./code_analysis.R | tee -a log.txt
+echo "END code_analysis.R" | tee -a log.txt
 
 # Matt: Generates SM figure S12 (prediction distances compared to truth)
-echo "BEGIN compare_predictions_truth.R" | tee log.txt
-Rscript ./compare_predictions_truth.R | tee log.txt
-echo "END compare_predictions_truth.R" | tee log.txt
+echo "BEGIN compare_predictions_truth.R" | tee -a log.txt
+Rscript ./compare_predictions_truth.R | tee -a log.txt
+echo "END compare_predictions_truth.R" | tee -a log.txt
 
 # Alex: Generates SM figures S19-22 (methods used in the Challenge)
-echo "BEGIN methods_used.R" | tee log.txt
-Rscript ./methods_used.R | tee log.txt
-echo "END methods_used.R" | tee log.txt
+echo "BEGIN methods_used.R" | tee -a log.txt
+Rscript ./methods_used.R | tee -a log.txt
+echo "END methods_used.R" | tee -a log.txt
 
 # Ian: Generates privacy redaction counts
-echo "BEGIN privacy_redaction_count.R" | tee log.txt
-Rscript ./privacy_redaction_count.R | tee log.txt
-echo "END privacy_redaction_count.R" | tee log.txt
+echo "BEGIN privacy_redaction_count.R" | tee -a log.txt
+Rscript ./privacy_redaction_count.R | tee -a log.txt
+echo "END privacy_redaction_count.R" | tee -a log.txt
 
 # Produces table S4 showing the number of non-missing cases for each outcome in the training, leaderboard, and holdout sets.
-echo "BEGIN num_nonmissing_cases.R" | tee log.txt
-Rscript ./num_nonmissing_cases.R | tee log.txt
-echo "END num_nonmissing_cases.R" | tee log.txt
+echo "BEGIN num_nonmissing_cases.R" | tee -a log.txt
+Rscript ./num_nonmissing_cases.R | tee -a log.txt
+echo "END num_nonmissing_cases.R" | tee -a log.txt
 
 # Remove Rplots.pdf
 rm -f ./Rplots.pdf
 
-echo "Replication code run complete." | tee log.txt
-echo "" | tee log.txt
+echo "Replication code run complete." | tee -a log.txt
+echo "" | tee -a log.txt
--- code/socius/run.sh
+++ code/socius/run.sh
@@ -4,29 +4,29 @@
 # Write a new log 
 rm -f log.txt
 touch log.txt
-echo "Log from run: $(date)" | tee log.txt
+echo "Log from run: $(date)" | tee -a log.txt
 
 # Produces missing data summary plots in Figure 3
-echo "BEGIN make_missing_data_graphs.R" | tee log.txt
-Rscript ./make_missing_data_graphs.R | tee log.txt
-echo "END make_missing_data_graphs.R" | tee log.txt
+echo "BEGIN make_missing_data_graphs.R" | tee -a log.txt
+Rscript ./make_missing_data_graphs.R | tee -a log.txt
+echo "END make_missing_data_graphs.R" | tee -a log.txt
 
 # Produces the table showing the number of non-missing cases for each outcome in the training, leaderboard, and holdout sets.
-echo "BEGIN num_nonmissing_cases.R" | tee log.txt
-Rscript ./num_nonmissing_cases.R | tee log.txt
-echo "END num_nonmissing_cases.R" | tee log.txt
+echo "BEGIN num_nonmissing_cases.R" | tee -a log.txt
+Rscript ./num_nonmissing_cases.R | tee -a log.txt
+echo "END num_nonmissing_cases.R" | tee -a log.txt
 
 # Reports the percent of entries made missing in our privacy and ethics audit.
 # We also cite this number in the community paper.
-echo "BEGIN privacy_redaction_count.R" | tee log.txt
-Rscript ./privacy_redaction_count.R | tee log.txt
-echo "END privacy_redaction_count.R" | tee log.txt
+echo "BEGIN privacy_redaction_count.R" | tee -a log.txt
+Rscript ./privacy_redaction_count.R | tee -a log.txt
+echo "END privacy_redaction_count.R" | tee -a log.txt
 
 # Produces the 3D tree visualization (Figure 5).
 # Run this script manually to generate the tree plot (must be manually saved)
-# echo "BEGIN tree_plot.R" | tee log.txt
-# Rscript ./tree_plot.R | tee log.txt
-# echo "END tree_plot.R" | tee log.txt
+# echo "BEGIN tree_plot.R" | tee -a log.txt
+# Rscript ./tree_plot.R | tee -a log.txt
+# echo "END tree_plot.R" | tee -a log.txt
 
 # Ensure leaderboard data is decompressed
 rm -rf /ffc_replication/data/leaderboard_nodupes/
@@ -34,8 +34,8 @@ yes | unzip /ffc_replication/data/leaderboard_nodupes.zip
 
 # Produces the empirical CDF of R^2 for each outcome and data set (Figure 6).
 # Also produces the scatterplot of R^2_Training and R^2_Holdout (Figure 7).
-echo "BEGIN r2_allSubmission_plots.R" | tee log.txt
-Rscript ./r2_allSubmission_plots.R | tee log.txt
-echo "END r2_allSubmission_plots.R" | tee log.txt
+echo "BEGIN r2_allSubmission_plots.R" | tee -a log.txt
+Rscript ./r2_allSubmission_plots.R | tee -a log.txt
+echo "END r2_allSubmission_plots.R" | tee -a log.txt
 
 rm -f Rplots.pdf
