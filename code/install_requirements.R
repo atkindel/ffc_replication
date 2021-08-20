2faa334ef79716f15449e72e02d82a1f4cbf7a3d
Alex Kindel <akindel@princeton.edu>
2021-08-20 10:29:55 -0700

    Deprecate package installation script

--- code/install_requirements.R
+++ code/install_requirements.R
@@ -1,6 +1,11 @@
-requirements <- c("tidyverse", "here", "foreach", "magrittr", "reshape2",
-                  "readstata13", "haven", "Amelia", "ranger", "quadprog",
-                  "broom", "gridExtra", "stargazer", "scales", "ggridges",
-                  "corrr", "tidytext", "foreign")
+# This code was used in March 2020 to install package dependencies for Docker v11
+# Some manual installation steps were involved
+# Re-installing today may yield slightly different results due to package updates since March 2020
+# All subsequent Docker versions are derived directly from v11 and do not rerun this script
 
-install.packages(requirements, repos="https://cloud.r-project.org")
+# requirements <- c("tidyverse", "here", "foreach", "magrittr", "reshape2",
+#                   "readstata13", "haven", "Amelia", "ranger", "quadprog",
+#                   "broom", "gridExtra", "stargazer", "scales", "ggridges",
+#                   "corrr", "tidytext", "foreign")
+# 
+# install.packages(requirements, repos="https://cloud.r-project.org")
